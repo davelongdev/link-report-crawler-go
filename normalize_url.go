b0ee925cd221e2877de4129f649077dd1fcf306b
dave long <davelongdev@gmail.com>
2024-09-02 18:27:30 -0400

    create normalizeURL.go and assocatied unit tests

--- normalize_url.go
+++ normalize_url.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
 func normalizeURL(rawURL string) (string, error) {
-  return "", nil
+
+  // call url.Parse and obtain parsedURL struct
+	parsedURL, err := url.Parse(rawURL)
+
+  // handle error if there is one
+	if err != nil {
+		return "", fmt.Errorf("couldn't parse URL: %w", err)
+	}
+  
+  // strip scheme
+	fullPath := parsedURL.Host + parsedURL.Path
+
+  // lowercase all characters
+	fullPath = strings.ToLower(fullPath)
+
+  // remove trailing slash
+	fullPath = strings.TrimSuffix(fullPath, "/")
+
+  // return normalized URL
+	return fullPath, nil
 }
--- normalize_url_test.go
+++ normalize_url_test.go
@@ -1,17 +1,32 @@
 package main
 
 import (
-  //"strings"
+  "strings"
   "testing"
 )
 
 func TestNormalizeURL(t *testing.T) {
+
+	// create a struct for each test case
 	tests := []struct {
+
+		// name of test case
 		name		string
+
+		// raw URL passed to the normalizeURL function
 		inputURL	string
+
+		// what the normalizeURL function is expected to return
 		expected	string
+
+		// if normalizeURL returns an error, what the error is expected to contain 
 		errorContains	string
 	}{
+		{
+			name:     "remove scheme http",
+			inputURL: "http://blog.boot.dev/path/",
+			expected: "blog.boot.dev/path",
+		},		
 		{
 			name:     "remove scheme https",
 			inputURL: "https://blog.boot.dev/path",
@@ -22,11 +37,6 @@ func TestNormalizeURL(t *testing.T) {
 			inputURL: "blog.boot.dev/path/",
 			expected: "blog.boot.dev/path",
 		},
-		{
-			name:     "remove scheme http",
-			inputURL: "http://blog.boot.dev/path/",
-			expected: "blog.boot.dev/path",
-		},		
 		{
 			name:     "lowercase capital letters",
 			inputURL: "BLOG.boot.dev/PATH",
@@ -43,38 +53,37 @@ func TestNormalizeURL(t *testing.T) {
 			expected:      "",
 			errorContains: "couldn't parse URL",
 		},
-        // can add more test cases here
+
+		// can add more test cases here
+		
 	}
 
-  // loop through test cases - i stands for index, tc stands for test case
+	// loop through test cases
 	for i, tc := range tests {
-		t.Run(tc.name, func(t *testing.T) {
-			actual, err := normalizeURL(tc.inputURL)
-			if err != nil {
-				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
-				return
-			}
-			if actual != tc.expected {
-				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
-			}
-		})
-	}
-}
 
-	for i, tc := range tests {
+		// create sub tests for each test case with t.Run
 		t.Run(tc.name, func(t *testing.T) {
+
+			// call the NormalizeURL function and store its return values
 			actual, err := normalizeURL(tc.inputURL)
+
+			// call t.errorf if error is returned and the error does not contain expected text
 			if err != nil && !strings.Contains(err.Error(), tc.errorContains) {
 				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
 				return
+
+			// call t.errorf if error is returned and there is no expected error text
 			} else if err != nil && tc.errorContains == "" {
 				t.Errorf("Test %v - '%s' FAIL: unexpected error: %v", i, tc.name, err)
 				return
+
+			// call t.errorf if no error is returned and error text was expected
 			} else if err == nil && tc.errorContains != "" {
 				t.Errorf("Test %v - '%s' FAIL: expected error containing '%v', got none.", i, tc.name, tc.errorContains)
 				return
 			}
 
+			// call t.Errorf if expected result is different than actual result
 			if actual != tc.expected {
 				t.Errorf("Test %v - %s FAIL: expected URL: %v, actual: %v", i, tc.name, tc.expected, actual)
 			}
